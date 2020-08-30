4cc44151ec1f4bf5a03a7c216e584e291c781a24
Graham Hoyes <accounts@grahamhoyes.com>
2020-08-30 16:33:32 -0400

    Try some other headers

--- app/app/settings/__init__.py
+++ app/app/settings/__init__.py
@@ -115,7 +115,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Script Name
-FORCE_SCRIPT_NAME = "mysite"
+FORCE_SCRIPT_NAME = "/mysite"
+USE_X_FORWARDED_HOST = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/

36cad0810afc436e8ae2bbbed9d0795b0fe5db39
Graham Hoyes <accounts@grahamhoyes.com>
2020-08-30 00:48:50 -0400

    First attempt at static files

--- app/app/settings/__init__.py
+++ app/app/settings/__init__.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")

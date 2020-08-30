4c42740dbf96fd4444631f4313227dbb828c24b7
Graham Hoyes <accounts@grahamhoyes.com>
2020-08-30 00:24:50 -0400

    Make view more efficient

--- app/app/views.py
+++ app/app/views.py
@@ -4,4 +4,6 @@ from app.models import AccessRecord
 
 def home(request):
     AccessRecord.objects.create()
-    return JsonResponse({"hits": AccessRecord.objects.count()})
+    # Get the most recent ID, which is much faster than doing .count() once
+    # someone writes a bot to curl your site in a loop 100k times
+    return JsonResponse({"hits": AccessRecord.objects.order_by("-id").first().id})
